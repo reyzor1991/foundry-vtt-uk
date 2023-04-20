2d7ffbafb138220d163c73ac225999a3fe607e94
Mykhailo Budiakov <mishabudjakov@gmail.com>
2023-04-20 16:48:16 +0300

    add babele for translate compendium 0.2.3

--- scripts/init.js
+++ scripts/init.js
@@ -1,15 +1,15 @@
 Hooks.once('init', async function () {
-
-    const moduleName = 'pfe2ukua'
+	
 	const systemTranslationsFolder = "modules/pfe2ukua/";
-	const pf2TranslationsFolder = systemTranslationsFolder + "pf2-lang/";
 	const moduleTranslationsFolder = systemTranslationsFolder + "modules/";
+	
+	const systemFiles = ["pf2-lang/pf2e.json", "pf2-lang/re-pf2e.json", "pf2-lang/action-pf2e.json"]
+	const moduleFiles = [
+		{"id":"babele", "path":"babele.json"}
+	]
 
-	const pf2Files = ["action-uk.json", "re-uk.json", "uk.json"]
-	const moduleFiles = []
-
-	game.settings.register(moduleName, 'translateSystem', {
-        name: "Translation: Ukrainian [PF2e]",
+	game.settings.register('pfe2ukua', 'translateSystem', {
+        name: "Переклад інтерфейсу PF2e для Foundry VTT українською мовою",
         scope: 'world',
         type: Boolean,
         config: true,
@@ -17,14 +17,14 @@ Hooks.once('init', async function () {
         restricted: true,
         requiresReload: true
     });
-
+	
 	moduleFiles.forEach(m =>{
-		let id = m.id;
+		let id = m.id;		
 		let module = game.modules.get(m.id);
-
+		
 		console.log("Checking mod: " + m.id);
 		if(module?.active){
-			game.settings.register(moduleName, "translateModule_" + m.id, {
+			game.settings.register("pfe2ukua", "translateModule_" + m.id, {
 				name: "Переклад модуля " + module.title,
 				scope: 'world',
 				type: Boolean,
@@ -37,18 +37,18 @@ Hooks.once('init', async function () {
 	});
 
     if (typeof libWrapper === "function") {
-        libWrapper.register(moduleName
+        libWrapper.register("pfe2ukua",
             "game.i18n._getTranslations",
             loadSelectedTranslations,
             "MIXED");
     }
     else {
         new Dialog({
-            title: "Выбор перевода",
+            title: "Вибір перекладу",
             content: `<p>Для работы модуля перевода необходимо активировать модуль <b>libWrapper</b></p>`,
             buttons: {
                 done: {
-                    label: "Хорошо",
+                    label: "ОК!",
                 },
             },
         }).render(true);
@@ -57,21 +57,21 @@ Hooks.once('init', async function () {
     async function loadSelectedTranslations(wrapped, lang) {
         const defaultTranslations = await wrapped(lang);
         const promises = [];
-
+		
 		if (game.i18n.lang != "uk")
 			return defaultTranslations;
-
-		if(game.settings.get(moduleName, "translateSystem")){
-			pf2Files.forEach(f =>{
-				promises.push(this._loadTranslationFile(pf2TranslationsFolder + f));
+		
+		if(game.settings.get("pfe2ukua", "translateSystem")){
+			systemFiles.forEach(f =>{
+				promises.push(this._loadTranslationFile(systemTranslationsFolder + f));
 			});
 		}
-
+		
 		moduleFiles.forEach(m =>{
-			let id = m.id;
+			let id = m.id;		
 			let module = game.modules.get(m.id);
-
-			if(module?.active && game.settings.get(moduleName, "translateModule_" + m.id)){
+		
+			if(module?.active && game.settings.get("pfe2ukua", "translateModule_" + m.id)){
 				promises.push(this._loadTranslationFile(moduleTranslationsFolder + m.path));
 			}
 		});
